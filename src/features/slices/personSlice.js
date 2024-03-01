0a38bb479d22e38318647e5da9fd32651adac718
StasAnanas666 <lapshin-5a@yandex.ru>
2024-03-01 21:23:19 +0300

    add store and personSlice

---
+++ src/features/slices/personSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../api/api";
+
+export const createPerson = createAsyncThunk(
+    "person/createPerson",
+    api.createPerson
+);
+export const updatePerson = createAsyncThunk(
+    "person/updatePerson",
+    api.updatePerson
+);
+export const getPerson = createAsyncThunk("person/getPerson", api.getPerson);
+
+const personSlice = createSlice({
+    name: "personState",
+    initialState: {
+        createStatus: false,
+        updateStatus: false,
+        currentPerson: [],
+        currentStatus: false,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createPerson.pending, (state) => {
+                state.createStatus = false;
+            })
+            .addCase(createPerson.fulfilled, (state, action) => {
+                //при успешном создании пользователя выводим в консоль инфо о нем, сохраняем его как текущего пользователя, переключаем статус создания пользователя на true
+                console.log(`Создан пользователь ${action.payload.name}`);
+                state.currentPerson = action.payload;
+                state.createStatus = true;
+            })
+            .addCase(updatePerson.pending, (state) => {
+                state.updateStatus = false;
+            })
+            .addCase(updatePerson.fulfilled, (state, action) => {
+                state.currentPerson[0].name = action.payload.name;
+                state.updateStatus = true;
+            })
+            .addCase(getPerson.pending, (state) => {
+                state.currentStatus = false;
+            })
+            .addCase(getPerson.fulfilled, (state, action) => {
+                state.currentStatus = true;
+                state.currentPerson = action.payload;
+            });
+    },
+});
+
+export const regState = (state) => state.personState;
+export default personSlice.reducer;
